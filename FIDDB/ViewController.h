8919a82b2b30f156f9586f388179ac5e62a37fba
fidetro <zykzzzz@hotmail.com>
2017-03-22 22:45:32 +0800

    Initial Commit

---
+++ FIDDB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FIDDB
+//
+//  Created by Fidetro on 2017/3/22.
+//  Copyright © 2017年 Fidetro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FIDDBTests/FIDDBTests.m
@@ -0,0 +1,39 @@
+//
+//  FIDDBTests.m
+//  FIDDBTests
+//
+//  Created by Fidetro on 2017/3/22.
+//  Copyright © 2017年 Fidetro. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FIDDBTests : XCTestCase
+
+@end
+
+@implementation FIDDBTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
